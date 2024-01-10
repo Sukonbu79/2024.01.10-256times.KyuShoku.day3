2d9684427a4069364f5eab90f665b086b46b111b
Sukonbu79 <149184637+Sukonbu79@users.noreply.github.com>
2024-01-11 02:08:42 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+'use strict';
+
+{
+// menu
+
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const list = document.getElementById("list");
+
+  open.addEventListener("click", () => {
+    list.classList.add("show");
+    close.classList.add("show");
+    open.classList.add("hide");
+});
+
+  close.addEventListener("click", () => {
+    list.classList.remove("show");
+    close.classList.remove("show");
+    open.classList.remove("hide");
+});
+
+// heroSlideShow
+
+const thumbnails = document.querySelectorAll(".thumb");
+const mainImage = document.getElementById("main-image");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+let activeIndex = 0;
+
+thumbnails[0].addEventListener('click', () => {
+  activeIndex = 0;
+  mainImage.src = thumbnails[0].src;
+  thumbnails[0].classList.add('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+});
+
+thumbnails[1].addEventListener('click', () => {
+  activeIndex = 1;
+  mainImage.src = thumbnails[1].src;
+  thumbnails[1].classList.add('active');
+  thumbnails[0].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+});
+
+thumbnails[2].addEventListener('click', () => {
+  activeIndex = 2;
+  mainImage.src = thumbnails[2].src;
+  thumbnails[2].classList.add('active');
+  thumbnails[0].classList.remove('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+});
+
+thumbnails[3].addEventListener('click', () => {
+  activeIndex = 3;
+  mainImage.src = thumbnails[3].src;
+  thumbnails[3].classList.add('active');
+  thumbnails[0].classList.remove('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+});
+
+thumbnails[4].addEventListener('click', () => {
+  activeIndex = 4;
+  mainImage.src = thumbnails[4].src;
+  thumbnails[4].classList.add('active');
+  thumbnails[0].classList.remove('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+});
+
+next.addEventListener('click', () => {
+  activeIndex++;
+  if (activeIndex === thumbnails.length) {
+    activeIndex = 0;
+  }
+  mainImage.src = thumbnails[activeIndex].src;
+  thumbnails[0].classList.remove('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+  thumbnails[activeIndex].classList.add('active');
+});
+
+prev.addEventListener('click', () => {
+  activeIndex--;
+  if (activeIndex < 0) {
+    activeIndex = thumbnails.length - 1;
+  }
+  mainImage.src = thumbnails[activeIndex].src;
+  thumbnails[0].classList.remove('active');
+  thumbnails[1].classList.remove('active');
+  thumbnails[2].classList.remove('active');
+  thumbnails[3].classList.remove('active');
+  thumbnails[4].classList.remove('active');
+  thumbnails[activeIndex].classList.add("active");
+});
+
+}
